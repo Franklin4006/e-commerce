b14ebbe000d700b6e00161a2bb802daaa22370c2
Franklin <habidasf@gmail.com>
2026-07-28 20:41:27 +0530

    ui correction

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -135,7 +135,7 @@
                 </div>
 
                 <button type="submit" class="btn btn-primary btn-pill">
-                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="22" y1="2" x2="11" y2="13"/><polygon points="22 2 15 22 11 13 2 9 22 2"/></svg>
+                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="22" y1="2" x2="11" y2="13"/><polygon points="22 2 15 22 11 13 2 9 22 2"/></svg>
                     Send Message
                 </button>
             </form>
